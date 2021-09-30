64b41e427f7ed16a7109f913ac4de4bd6bcf7d83
Sivareddy <sivareddychidipudi@gmail.com>
2021-09-30 10:07:22 +0530

    modal is completed

--- src/Ecommerce.js
+++ src/Ecommerce.js
@@ -1,5 +1,9 @@
 import React,{useState} from 'react'
 import Data from './data.json'
+import './Ecommerce.css'
+import Modal from 'react-modal'
+
+Modal.setAppElement("#root")
 
 const Ecommerce = (props) =>{
 
@@ -9,8 +13,11 @@ const Ecommerce = (props) =>{
     const [product,setProduct] = useState('')
     const [cartdata,setCartData] = useState([])
     const [formerrors,setFormerrors] = useState({})
+    const [toggle,setToggle] = useState(false)
+     const [modalisopen, setModalIsOpen] = useState(false)
     const errors = {}
     let result = []
+    console.log(toggle)
     const optioncategory = data.map((ele)=>{
        
         if(!result.includes(ele.category))
@@ -62,7 +69,7 @@ const Ecommerce = (props) =>{
             }
             else 
             {
-                errors.stock='stock not available'
+                alert('stock is not available')
             }
             }
             return formdata
@@ -97,7 +104,7 @@ const Ecommerce = (props) =>{
                 return {...ele,available : ele.available-1}
                 }
                 else {
-                    errors.stock = 'stock not available'
+                    alert('stock is not available')
                     return {...ele}
                 }
             }
@@ -131,7 +138,9 @@ const Ecommerce = (props) =>{
         if(Object.keys(errors).length==0)
         {
             setFormerrors({})
-            console.log(cartdata)
+            console.log(cartdata,'cartdata')
+            setToggle(!toggle)
+            setModalIsOpen(true)
             setCategory('')
         }
         else {
@@ -161,14 +170,27 @@ const Ecommerce = (props) =>{
 
     }
 
-    console.log(errors)
+     const handleChangeClose = () => {
+    setModalIsOpen(false);
+    setToggle(false);
+    setCartData([])
+  };
+
+   const total = cartdata.reduce((count,ele)=>{
+                
+                count=count+ele.quantity*ele.price
+                return count 
+            },0)
+            
+        
+
 
 
     return (
         <div style={{color : 'blue',display:'flex',marginLeft:'8%',justifyContent : 'space-around'}}>
         <div>
             <h1>E-commerce</h1>
-            <select value={category} onChange={handleChangecategory}>
+            <select value={category} onChange={handleChangecategory} style={{width:'100%'}}>
                 <option value=''>Select</option>
                 {result.map((ele,i)=>{
                     return <option key={i} value={ele}>{ele}</option>
@@ -177,16 +199,18 @@ const Ecommerce = (props) =>{
             <br/>
             {formerrors&&<span>{formerrors.category}</span>}
             <br/>
-            <select value={product} onChange={handleChangeProduct}>
+            <select value={product} onChange={handleChangeProduct} style={{width:'100%'}}>
                 <option value=''>Select</option>
                 {filterdata.map((ele,i)=>{
                     return <option value={ele.name} key={i}>{ele.name}</option>
                 })}
             </select>
             <br/>
+            <br/>
             <button onClick={handleChangeaddcart}>Add cart</button>
             <br/>
             {errors&&<span>{errors.stock}</span>}
+            <br/>
             <button onClick={handleSubmit}>submit</button>
             </div>
             <div>
@@ -221,7 +245,41 @@ const Ecommerce = (props) =>{
 
             )}
             </div>
+           <div>
+           {toggle && <Modal isOpen={modalisopen}
+                              shouldCloseOnOverlayClick={false}
+                              onRequestClose={() => {
+                              setModalIsOpen(false);
+                               }}>
+                                 <table>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cartdata.map((ele,i)=>{
+                        return (
+                            <tr key={i}>
+                                <td>{ele.name}</td>
+                                <td>{ele.quantity}</td>
+                                <td>{ele.price}</td>
+                                <td>{ele.quantity*ele.price}</td>
+                            </tr>
+                        )
+                        
+                    })}
+                </tbody>
+            </table>
+            <h1>Total : {total}</h1>
            
+
+                               <button onClick={handleChangeClose}>close</button>
+                      </Modal>}
+           </div>
         </div>
     )
 }
